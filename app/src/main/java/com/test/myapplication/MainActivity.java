1e0abfefa9c61b771ae65bf82966046c98e7f181
ibrahiemhss <kekoreko3@gmail.com>
2020-07-18 11:18:18 +0200

    update

--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.test.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
