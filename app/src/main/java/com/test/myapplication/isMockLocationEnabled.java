11d65d42b58ad261d051e0a43b1ee8896be4eec4
Jafar jabr <jafar.jabr@punct.ro>
2020-07-18 12:42:48 +0300

    fast update

---
+++ app/src/main/java/com/test/myapplication/isMockLocationEnabled.java
@@ -0,0 +1,53 @@
+package com.test.myapplication;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+
+import java.util.List;
+
+public class isMockLocationEnabled {
+    public static boolean check(Context context) {
+        if(Build.VERSION.SDK_INT <= 22 && !Settings.Secure.getString(context.getContentResolver(),
+                Settings.Secure.ALLOW_MOCK_LOCATION).equals("0")) {
+            return true;
+        } else {
+            return areThereMockPermissionApps(context);
+        }
+    }
+
+    private static boolean areThereMockPermissionApps(Context context) {
+        int count = 0;
+
+        PackageManager pm = context.getPackageManager();
+        List<ApplicationInfo> packages =
+                pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo applicationInfo : packages) {
+            try {
+                PackageInfo packageInfo = pm.getPackageInfo(applicationInfo.packageName,
+                        PackageManager.GET_PERMISSIONS);
+
+                // Get Permissions
+                String[] requestedPermissions = packageInfo.requestedPermissions;
+
+                if (requestedPermissions != null) {
+                    for (String requestedPermission : requestedPermissions) {
+                        if (requestedPermission
+                                .equals("android.permission.ACCESS_MOCK_LOCATION")
+                                && !applicationInfo.packageName.equals(context.getPackageName())) {
+                            count++;
+                        }
+                    }
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e("Got exception " , e.getMessage());
+            }
+        }
+        return count > 0;
+    }
+}
