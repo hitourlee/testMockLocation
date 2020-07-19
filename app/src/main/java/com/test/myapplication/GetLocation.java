feb3b65844cddd75272c2e4aaaf6aa59a23476fb
Jafar jabr <jafar.jabr@punct.ro>
2020-07-19 16:53:51 +0300

    fast update

---
+++ app/src/main/java/com/test/myapplication/GetLocation.java
@@ -0,0 +1,34 @@
+package com.test.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+
+import androidx.core.app.ActivityCompat;
+
+import java.util.List;
+
+
+public class GetLocation {
+
+    public static Location getLastKnownLocation(Context context) {
+        LocationManager mLocationManager;
+        mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            }
+            Location l = mLocationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
+    }
+}
--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package com.test.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void getPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                1);
+        ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                1);
+        ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "MockS = "+isMockLocationEnabled.check(mContext),
+                getPermission();
+                Location currentLocation = GetLocation.getLastKnownLocation(mContext);
+                Toast.makeText(mContext, "MockS = "+ currentLocation.getAltitude() + " and " + currentLocation.getLongitude(),
                         Toast.LENGTH_LONG).show();
             }
         });
